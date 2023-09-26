a1b4c530b23f313c691c1bb034d7dfcc655a4010
Kupigabi <gabor.kupeczki@gmail.com>
2023-09-26 17:22:35 +0200

    changed script2.js content

--- frontend/script2.js
+++ frontend/script2.js
@@ -1,1 +1,11 @@
-console.log("script2")
+function homeWork(text) {
+    console.log(text)
+}
+homeWork("I did it")
+
+function maths(a, b, c, d, e){
+    let sum = ((a + b) - (c * d)) / e
+    return sum
+}
+
+homeWork(maths(1,2,3,4,5))
