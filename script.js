91e671daeb0959a723c560dba7e1831d007b8ef8
Kupigabi <gabor.kupeczki@gmail.com>
2023-09-26 13:03:18 +0200

    made logger and another function

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let variable = "kis"
-let variable2 = "macska"
+function logger(param) {
+    console.log(param)
+}
 
-let number = 5
-let number2 = 10
+logger("kismacska")
 
-console.log("100" + "2");
-console.log(100 + 2)
-console.log("variable")
-console.log(variable + variable2)
+function sumOfTwoNumbers(number1, number2){
+    // console.log(number1 + number2)
+    let sum = number1 + number2
+    return sum
+}
+
+/* let result = sumOfTwoNumbers(2,4)
+logger(result)
+ */
+
+logger(sumOfTwoNumbers(5, 10))
