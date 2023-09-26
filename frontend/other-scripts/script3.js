9f7b61b276a7c4664555183ce0064a2412ff8141
Kupigabi <gabor.kupeczki@gmail.com>
2023-09-26 17:29:23 +0200

    changed script3.js content

--- frontend/other-scripts/script3.js
+++ frontend/other-scripts/script3.js
@@ -1,1 +1,13 @@
-console.log("script3")
+function test(something){
+    console.log(something)
+}
+
+test("Próba")
+
+function multi(a, b, c){
+    let sum = a * b * c
+    return sum
+}
+
+console.log(multi(1,2,3))
+// test(multi(1,2,3))
